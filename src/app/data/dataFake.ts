216aaddd9219def5f8de711f6694f3cfdfdc77d8
davidplay7 <davidk1k3k@gmail.com>
2024-01-09 17:29:48 -0300

    Finalizando as paginas dinamicas

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,27 @@
+export const dataFake = [
+    {
+        "id":"1",
+        "title": "GitHub - angular/angular: Deliver web apps with confidence 🚀",
+        "description": "Versão ^12.0.0 Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi eum sit, nulla ullam nam quos, numquam iste mollitia dolore unde atque, dolores ab? Eaque totam placeat est debitis exercitationem porro?",
+        "photoCover": "https://repository-images.githubusercontent.com/24195339/d4194dc2-d880-43f7-960c-ea30e05c6531"
+    },
+    {
+        "id":"2",
+        "title": "Gal Gadot-Varsano é uma atriz e modelo israelense",
+        "description": "",
+        "photoCover": "https://images.bauerhosting.com/legacy/empire-images/articles/56f40701264e015902d56faf/wonder-woman-crop.jpg?ar=16%3A9&fit=crop&crop=top&auto=format&w=1440&q=80"
+    },
+    {
+        "id":"3",
+        "title": "Why We’re Still Waiting on 'Sherlock Holmes 3'",
+        "description": "",
+        "photoCover": "https://static0.colliderimages.com/wordpress/wp-content/uploads/2023/06/sherlock-holmes-3-robert-downey-jr-jude-law.jpg"
+    },
+    {
+        "id":"4",
+        "title": "GTA 6: 10 carros que estarão no game para PS5 e Xbox Series X",
+        "description": "",
+        "photoCover": "https://cdn.autopapo.com.br/box/uploads/2023/12/06131947/poster-gta-6-1-732x488.jpg"
+    },
+    
+];
